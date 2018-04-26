64f842b7684470528e87480de1267f647996f452
Karen <dev.kaskar2008@gmail.com>
2018-04-26 05:35:58 +0300

    preset

---
+++ lib/vueport.vue
@@ -0,0 +1,35 @@
+<template>
+
+</template>
+
+<script>
+
+export default {
+  name: 'vueport',
+  props: {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  created() {
+
+  },
+  mounted () {
+
+  },
+  beforeDestroy () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
+
